7ce1142f319bd28276a4677a8acd0be5b340b95d
callmehai <kdodjeksdkkd@gmail.com>
2025-05-22 16:38:58 +0700

    Remove tracked files ignored by .gitignore

--- HospitalManagement/HospitalManagement/obj/Release/net8.0/HospitalManagement.AssemblyInfo.cs
+++ HospitalManagement/HospitalManagement/obj/Release/net8.0/HospitalManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d612b28a1feb80458c4cf96c326a4ad19946a7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28442d7256e5d0673039afafbe53cfdcab2b7c49")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
