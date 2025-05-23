b29bac671c889e904d219b0b573e512c8e437292
callmehai <kdodjeksdkkd@gmail.com>
2025-05-24 03:12:42 +0700

    Demo Temp
    Demo temp

---
+++ HospitalManagement/HospitalManagement/Models/LoginView.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalManagement.Models
+{
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
---
+++ HospitalManagement/HospitalManagement/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagement.Models
+{
+    public class User : Controller
+    {
+        public IActionResult ViewProfile()
+        {
+            // Load profile data from database
+            return View();
+        }
+
+        public IActionResult UpdateProfile()
+        {
+            // Load profile data to edit
+            return View();
+        }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
