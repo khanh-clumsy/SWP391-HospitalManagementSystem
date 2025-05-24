445c9cf389fb715c92ba3fe582ec4c3b15fd3b30
Phạm Gia Khánh <phamgiakhanh0709@gmail.com>
2025-05-24 17:38:22 +0700

    cm

--- HospitalManagement/HospitalManagement/Controllers/PatientController.cs
+++ HospitalManagement/HospitalManagement/Controllers/PatientController.cs
@@ -13,5 +13,10 @@ namespace HospitalManagement.Controllers
         {
             return View();
         }
+
+        public IActionResult RequestConsultant()
+        {
+            return View();
+        }
     }
 }
