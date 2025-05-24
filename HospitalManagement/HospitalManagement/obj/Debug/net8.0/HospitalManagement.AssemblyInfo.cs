9fd308812677d7874a95a40542ade4be9b404cea
Phạm Gia Khánh <phamgiakhanh0709@gmail.com>
2025-05-24 15:37:20 +0700

    a

--- HospitalManagement/HospitalManagement/obj/Debug/net8.0/HospitalManagement.AssemblyInfo.cs
+++ HospitalManagement/HospitalManagement/obj/Debug/net8.0/HospitalManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b29bac671c889e904d219b0b573e512c8e437292")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13180d75b1a61e40ba2e43c759fd2b0e5f2e83f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
