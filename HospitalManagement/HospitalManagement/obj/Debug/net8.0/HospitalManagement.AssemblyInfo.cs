0c6e3698b39c66f1d396e99c2a97188b2e0991ad
gumarr <163157617+gumarr@users.noreply.github.com>
2025-05-27 12:26:55 +0700

    Update ViewDoctors

--- HospitalManagement/HospitalManagement/obj/Debug/net8.0/HospitalManagement.AssemblyInfo.cs
+++ HospitalManagement/HospitalManagement/obj/Debug/net8.0/HospitalManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+541c4d3e3c8c291bf1280273a83bd1cbbcd4fae6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503b4c444e28d1f13c9078dba120ee1b1174c0ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
