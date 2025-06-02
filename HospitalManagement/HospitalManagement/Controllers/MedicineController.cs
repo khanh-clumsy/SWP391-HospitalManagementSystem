d3763e7ca05abd977eee4745a18ee71b84d368b0
Phạm Gia Khánh <phamgiakhanh0709@gmail.com>
2025-06-02 21:12:41 +0700

    Thêm MedicineList và MedicineController

---
+++ HospitalManagement/HospitalManagement/Controllers/MedicineController.cs
@@ -0,0 +1,18 @@
+﻿using HospitalManagement.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagement.Controllers
+{
+    public class MedicineController : Controller
+    {
+        private readonly HospitalManagementContext _context;
+        public MedicineController(HospitalManagementContext context) {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
