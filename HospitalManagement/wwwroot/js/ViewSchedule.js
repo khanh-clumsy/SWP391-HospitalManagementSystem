e3e3655469fa2479cd32438267482e1d758b5656
Phạm Gia Khánh <phamgiakhanh0709@gmail.com>
2025-07-23 21:36:42 +0700

    Sửa giao diện

--- HospitalManagement/wwwroot/js/ViewSchedule.js
+++ HospitalManagement/wwwroot/js/ViewSchedule.js
@@ -3,9 +3,9 @@
 function updateSchedule(newyear = 0) {
     let year = newyear !== 0 ? newyear : document.getElementById("yearDropdown").value;
     let weekStart = newyear !== 0
-    ? (new Date(newyear, 0, 1)).toLocaleDateString().split('T')[0]
-    : document.getElementById("weekDropdown").value;
-    
+        ? (new Date(newyear, 0, 1)).toLocaleDateString().split('T')[0]
+        : document.getElementById("weekDropdown").value;
+
     $.ajax({
         url: '/Schedule/GetScheduleTable',
         type: 'GET',
@@ -43,7 +43,7 @@ function getFirstMondayOfYear(year) {
     return firstMonday.toISOString().split('T')[0];
 }
 
-function openSlotModal(el){
+function openSlotModal(el) {
     const scheduleId = el.dataset.id;
     const day = el.dataset.day;
     const start = el.dataset.start;
@@ -55,23 +55,23 @@ function openSlotModal(el){
 
     // console.log(isToday);
 
-	document.getElementById("modalDate").textContent = day;
-	document.getElementById("modalTime").textContent = `${start} - ${end}`;
-	document.getElementById("modalRoom").textContent = roomName;
+    document.getElementById("modalDate").textContent = day;
+    document.getElementById("modalTime").textContent = `${start} - ${end}`;
+    document.getElementById("modalRoom").textContent = roomName;
     document.getElementById("modalSlot").textContent = slot;
 
-	document.getElementById("modalStatus").value = status;
-	document.getElementById("modalScheduleId").value = scheduleId;
+    document.getElementById("modalStatus").value = status;
+    document.getElementById("modalScheduleId").value = scheduleId;
 
     document.getElementById("modalStatus").disabled = !isToday;
-	document.getElementById("saveStatusBtn").disabled = !isToday;
+    document.getElementById("saveStatusBtn").disabled = !isToday;
 
     const modal = new bootstrap.Modal(document.getElementById('slotModal'));
     modal.show();
 }
 
-function updateScheduleStatus(){
-   
+function updateScheduleStatus() {
+
     const status = document.getElementById('modalStatus').value;
     const scheduleId = document.getElementById('modalScheduleId').value;
 
